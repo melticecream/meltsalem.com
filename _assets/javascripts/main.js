fb762452fb967769a4e4997f69a0956cf10856de
Dave Kroondyk <davekaro@gmail.com>
2017-07-08 19:45:11 -0400

    only one active box at a time with mobile support

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,9 +1,23 @@
 //= require jquery-3.2.1.min
 
+function addActive() {
+  removeActive();
+
+  $(this).children('p').addClass('dn');
+  $(this).children('div').removeClass('dn');
+  $(this).addClass('bg-red white active');
+}
+
+function removeActive() {
+  var $active = $('.active');
+  $active.children('div').addClass('dn');
+  $active.children('p').removeClass('dn');
+  $active.removeClass('bg-red white active');
+}
+
 $(function() {
-  $('.toggle').on('click', function() {
-    $(this).toggleClass('bg-red white');
-    $(this).children('p').toggleClass('dn');
-    $(this).children('div').toggleClass('dn');
-  });
+  $('.toggle').hover(addActive, removeActive);
+
+  // also do the click event to support mobile
+  $('.toggle').click(addActive);
 });
