0de83a9f459901d7211b88f4aef3c0e06ebb260b
Dave Kroondyk <davekaro@gmail.com>
2017-10-12 08:42:35 -0400

    switch from jquery to umbrella

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,25 +1,26 @@
-//= require jquery-3.2.1.min
+//= require umbrella.min
 
 function addActive() {
   removeActive();
 
-  $(this).children('p').addClass('dn');
-  $(this).children('div').removeClass('dn');
-  $(this).addClass('bg-dark-gray white active');
-  $(this).removeClass('bg-flavor');
+  u(this).children('p').addClass('dn');
+  u(this).children('div').removeClass('dn');
+  u(this).addClass('bg-dark-gray white active');
+  u(this).removeClass('bg-flavor');
 }
 
 function removeActive() {
-  var $active = $('.active');
+  var $active = u('.active');
   $active.children('div').addClass('dn');
   $active.children('p').removeClass('dn');
   $active.addClass('bg-flavor');
   $active.removeClass('bg-dark-gray white active');
 }
 
-$(function() {
-  $('.toggle').hover(addActive, removeActive);
+document.addEventListener('DOMContentLoaded', function() {
+  u('.toggle').on('mouseenter', addActive);
+  u('.toggle').on('mouseleave', removeActive);
 
   // also do the click event to support mobile
-  $('.toggle').click(addActive);
+  u('.toggle').on('click', addActive);
 });
